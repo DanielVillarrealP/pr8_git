49a509b196b407b5f5da8ae8a859ad9e45541fad
RLVEGA <errevega1991@gmail.com>
2020-02-27 17:04:39 +0100

    Aportación de Rubén

--- HelloWorldGit/src/Main.java
+++ HelloWorldGit/src/Main.java
@@ -3,6 +3,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
+
+		methodRuben();
+	}
+
+	public void methodRuben() {
+		System.out.println("Hola mundo, cambiado por Rubén Vega");
 	}
 
 }
